e80872c79419b3365167b2b1fabcee62bbfec09a
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-08-30 13:18:34 -0400

    feat(): (Add validations for document)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  before_save :sanitize_document
+
   validates :document, :legal_name, :trade_name, presence: true
   validates :document, :legal_name, uniqueness: { case_sensitive: false }
+  validate :valid_document
+
+  private
+
+  def valid_document
+    errors.add :base, I18n.t('company.errors.document') unless CNPJ.valid?(document)
+  end
+
+  def sanitize_document
+    document.gsub!(/[^0-9]/, '')
+  end
 end
